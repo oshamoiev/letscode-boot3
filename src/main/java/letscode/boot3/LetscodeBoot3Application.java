5fc1cfc51fa8923117f8ecbbf7e3857401a66ce3
Oleksandr Shamoiev <wamoev@gmail.com>
2023-06-15 14:17:53 +0300

    Initialize Spring Boot app

---
+++ src/main/java/letscode/boot3/LetscodeBoot3Application.java
@@ -0,0 +1,13 @@
+package letscode.boot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetscodeBoot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetscodeBoot3Application.class, args);
+    }
+
+}
---
+++ src/test/java/letscode/boot3/LetscodeBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package letscode.boot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetscodeBoot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
