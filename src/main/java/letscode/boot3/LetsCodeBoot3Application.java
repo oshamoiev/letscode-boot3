850c60eb28e319f38cbd699b5ed14e6f8c832413
Oleksandr Shamoiev <wamoev@gmail.com>
2023-06-20 13:07:08 +0300

    Rename main class

--- src/main/java/letscode/boot3/LetsCodeBoot3Application.java
+++ src/main/java/letscode/boot3/LetsCodeBoot3Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LetscodeBoot3Application {
+public class LetsCodeBoot3Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(LetscodeBoot3Application.class, args);
+        SpringApplication.run(LetsCodeBoot3Application.class, args);
     }
 
 }
--- src/test/java/letscode/boot3/LetsCodeBoot3ApplicationTests.java
+++ src/test/java/letscode/boot3/LetsCodeBoot3ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LetscodeBoot3ApplicationTests {
+class LetsCodeBoot3ApplicationTests {
 
     @Test
     void contextLoads() {
